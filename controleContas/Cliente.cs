a3a9a6f61992b7e18090f5dbe579bb91443a3dc5
jppaciello <120607285+jppaciello@users.noreply.github.com>
2023-09-12 15:20:25 -0300

    Update Cliente.cs

--- controleContas/Cliente.cs
+++ controleContas/Cliente.cs
@@ -22,4 +22,34 @@ namespace controleContas
         }
     }
 
+    public class Banco 
+{
+    public int Numero { get; set; }
+    public string Nome { get; set; }
+
+    public Banco() { }
+
+    public Banco(string nome, int numero)
+    {
+        this.Numero = numero;
+        this.Nome = nome;
+    }
+}
+
+    public class Agencia
+{
+    public int Numero { get; set; }
+    public string CEP { get; set; }
+    public string Telefone { get; set; }
+
+    public Agencia() { }
+
+    public Agencia(int numero,string CEP, string telefone)
+    {
+        this.Numero = numero;
+        this.CEP = CEP;
+        this.Telefone = telefone;
+    }
+}
+
 }
